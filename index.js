a7ff2dea39d4aac97800b19f4f3ca1a152b98bb7
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-02-13 19:56:50 -0600

    Adds logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code away!
+const express = require('express');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
+const { logger } = require('./utils/logger');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter);
+server.use('/api/users/posts', postRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h1>🚀</h1>')
+})
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
---
+++ utils/logger.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logger
+}
+
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.url} at ${new Date}`)
+  next();
+}
